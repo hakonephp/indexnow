311b12f6897cb4f447664c6fdd31af3784fc0b14
USAMI Kenta <tadsan@zonu.me>
2026-08-19 03:12:40 +0900

    Kill remaining Infection mutants with a subclass

--- tests/IndexNowTest.php
+++ tests/IndexNowTest.php
@@ -10,6 +10,8 @@ use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\UsesClass;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\RequestInterface as HttpRequest;
+use Psr\Http\Message\ResponseInterface as HttpResponse;
 
 #[CoversClass(IndexNow::class)]
 #[UsesClass(IndexNowException::class)]
@@ -93,6 +95,51 @@ final class IndexNowTest extends TestCase
 
         self::assertCount(1, $http->receivedRequests);
         self::assertSame('api.indexnow.org', $http->receivedRequests[0]->getUri()->getHost());
+    }
+
+    public function test_subclass_can_customize_protected_request_hooks(): void
+    {
+        $http = $this->clientWithStatus(200);
+        $subject = new readonly class($http) extends IndexNow {
+            #[\NoDiscard]
+            protected function createRequest(string $method, string $url): HttpRequest
+            {
+                return parent::createRequest($method, $url)
+                    ->withHeader('X-Hook-Create', '1');
+            }
+
+            #[\NoDiscard]
+            protected function appendDefaultRequestHeaders(HttpRequest $request): HttpRequest
+            {
+                return parent::appendDefaultRequestHeaders($request)
+                    ->withHeader('X-Hook-Headers', '1');
+            }
+
+            /**
+             * @param array<string, mixed> $payload
+             */
+            #[\NoDiscard]
+            protected function appendJsonPayload(HttpRequest $request, array $payload): HttpRequest
+            {
+                return parent::appendJsonPayload($request, $payload)
+                    ->withHeader('X-Hook-Json', '1');
+            }
+
+            protected function sendRequest(HttpRequest $request): HttpResponse
+            {
+                return parent::sendRequest($request)
+                    ->withHeader('X-Hook-Send', '1');
+            }
+        };
+
+        $response = $subject->submitList('www.example.com', 'secret', ['https://www.example.com/url1']);
+
+        self::assertCount(1, $http->receivedRequests);
+        $request = $http->receivedRequests[0];
+        self::assertSame(['1'], $request->getHeader('X-Hook-Create'));
+        self::assertSame(['1'], $request->getHeader('X-Hook-Headers'));
+        self::assertSame(['1'], $request->getHeader('X-Hook-Json'));
+        self::assertSame(['1'], $response->getHeader('X-Hook-Send'));
     }
 
     /**
